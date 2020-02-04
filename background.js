801d9e63a92aa254ecc22b0603068300b2c6b131
Valentin Brosseau <c4software@gmail.com>
2020-02-04 14:56:26 +0100

    Use content_scripts instead of background service inject

--- background.js
+++ background.js
@@ -1,37 +1,32 @@
 var pattern = /^((http|https):\/\/)/;
-tab_already_injected = false;
-
-try{
-  chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
-
-    if(tab_already_injected) {
+try {
+  chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    if (!pattern.test(tab.url)) {
+      chrome.browserAction.setBadgeText({ text: "Disabled", tabId: tab.id });
       return;
     }
-
-    if(!pattern.test(tab.url)) {
-      chrome.browserAction.setBadgeText({text: "Disabled", tabId: tab.id});
-      return; 
-    }
-
-    if (changeInfo.status == 'complete' && tab.status == 'complete' && tab.url != undefined) {
-      chrome.tabs.get(tabId, function(tab) {
-        tab_already_injected = true;
-        chrome.tabs.executeScript(tabId, {file: 'inject.js'});
-      });
-    }
-  })
-} catch(err) {
+  });
+} catch (err) {
   // IGNORED
   console.log(err);
 }
 
 // accept messages from background
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    chrome.browserAction.setBadgeText({text: request.error, tabId: sender.tab.id});
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  chrome.browserAction.setBadgeText({
+    text: request.error,
+    tabId: sender.tab.id
+  });
 
-    if(request.error !== "0"){
-        chrome.browserAction.setBadgeBackgroundColor({color: "#F00", tabId: sender.tab.id});
-    } else {
-      chrome.browserAction.setBadgeBackgroundColor({color: "#585858", tabId: sender.tab.id});
-    }
+  if (request.error !== "0") {
+    chrome.browserAction.setBadgeBackgroundColor({
+      color: "#F00",
+      tabId: sender.tab.id
+    });
+  } else {
+    chrome.browserAction.setBadgeBackgroundColor({
+      color: "#585858",
+      tabId: sender.tab.id
+    });
+  }
 });
