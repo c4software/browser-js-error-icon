9feb547318d2e504c25c60e7cbe5977d682ef910
Valentin Brosseau <c4software@gmail.com>
2020-02-05 21:17:43 +0100

    Fix labels

--- popup.js
+++ popup.js
@@ -1,7 +1,10 @@
 document.addEventListener("DOMContentLoaded", event => {
   chrome.tabs.getSelected(null, tab => {
     chrome.browserAction.getBadgeText({ tabId: tab.id }, result => {
-      if (result) {
+      if (result == "Disabled") {
+        document.body.innerHTML = "Disabled";
+        document.body.style.cssText = "background-color: #356DF2;";
+      } else if (result) {
         document.body.style.cssText = "background-color: red;";
         document.body.innerHTML = `${result} error${result > 1 ? "s" : ""}`;
       } else {
