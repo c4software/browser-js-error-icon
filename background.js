931871a78bdbd62ee5af6f7ca276d4b692a3748a
Valentin Brosseau <c4software@gmail.com>
2020-02-07 15:56:04 +0100

    Cleaup code

--- background.js
+++ background.js
@@ -1,17 +1,3 @@
-var pattern = /^((http|https):\/\/)/;
-try {
-  chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    if (!pattern.test(tab.url)) {
-      chrome.browserAction.setBadgeText({ text: "Disabled", tabId: tab.id });
-      return;
-    }
-  });
-} catch (err) {
-  // IGNORED
-  console.log(err);
-}
-
-// accept messages from background
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   chrome.browserAction.setBadgeText({
     text: request.error,
